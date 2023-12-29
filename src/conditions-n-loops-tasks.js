ce867dd18c7e91904aad2a8dcb91714151f0912d
Anzhalika Turlak <turlak.by@gmail.com>
2023-12-29 14:57:48 +0300

    feat: done ten tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -265,8 +265,13 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -284,8 +289,16 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let number = num;
+  while (number > -1) {
+    const n = number % 10;
+    if (n === digit) {
+      return true;
+    }
+    number = Math.floor(number / 10) || -1;
+  }
+  return false;
 }
 
 /**
@@ -301,10 +314,22 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 1; i < arr.length; i += 1) {
+    let sumFirstHalf = 0;
+    for (let j = i - 1; j >= 0; j -= 1) {
+      sumFirstHalf += arr[j];
+    }
+    let sumLastHalf = 0;
+    for (let k = i + 1; k < arr.length; k += 1) {
+      sumLastHalf += arr[k];
+    }
+    if (sumFirstHalf === sumLastHalf) {
+      return i;
+    }
+  }
+  return -1;
 }
-
 /**
  * Generates a spiral matrix of a given size, filled with numbers in ascending order starting from one.
  * The direction of filling with numbers is clockwise.
